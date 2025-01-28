a9d6b149c3aa8d8d8a24258c3d495c3e4659af8e
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-01-28 13:50:29 +0100

    added Rate Limiting in Program.cs

--- Hiptobesquare/Hiptobesquare/Program.cs
+++ Hiptobesquare/Hiptobesquare/Program.cs
@@ -2,11 +2,28 @@
 using Hiptobesquare;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Rate Limiting to restrict the number of requests to the API
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 30,
+                Window = TimeSpan.FromSeconds(10)
+            }
+        ));
+});
+
 builder.Services.AddSingleton<DataManager>();
 builder.Services.AddSingleton<SquareService>();
 builder.Services.AddTransient<Startup>();
 
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 using (var scope = app.Services.CreateScope())
 {
